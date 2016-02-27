0ec9e5ea347c6494323826fd1747e66de85607d7
Grace Turke <grace.turke@gmail.com>
2016-02-27 12:42:39 -0500

    Sets up django framework, sets up templates

---
+++ hello_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/graceturke/Documents/Projects/myhellowebapp/hello_venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
